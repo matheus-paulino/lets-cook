7261925a199514a16fc9385fa51def1257a9827e
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-08 23:20:02 -0300

    set theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.pinkAccent,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6:
+                  TextStyle(fontSize: 20.0, fontFamily: 'RobotoCondensed'),
+            ),
       ),
       home: CategoriesScreen(),
     );
