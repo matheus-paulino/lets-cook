60082bdf6ef1349449407d2344d622aee523da1c
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-09 22:26:29 -0300

    show meal with your category

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
 
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Recipes per category'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Text(
+            categoryMeals[index].title,
+          );
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
