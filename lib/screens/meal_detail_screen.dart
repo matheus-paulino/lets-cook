e44433b5193b19536cc3563e972f188a6931f094
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-14 13:40:16 -0300

    meal detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -11,8 +11,56 @@ class MealDetailSreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Center(
-        child: Text('Details of recipe'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10.0),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 200,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.all(5),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                borderRadius: BorderRadius.circular(10)),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5.0,
+                      horizontal: 10,
+                    ),
+                    child: Text(
+                      meal.ingredients[index],
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  color: Theme.of(context).accentColor,
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
