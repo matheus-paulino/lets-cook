2b3dd6ec1fbbeb5e7c657114e52c22c9d49c7886
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-14 21:26:51 -0300

    navigate to settings screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'screens/settings_screen.dart';
 import 'screens/tabs_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'utils/app_routes.dart';
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (_) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (_) => CategoriesMealsScreen(),
-        AppRoutes.MEAL_DETAIL: (_) => MealDetailSreen()
+        AppRoutes.MEAL_DETAIL: (_) => MealDetailSreen(),
+        AppRoutes.SETTINGS: (_) => SettingsScreen()
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
