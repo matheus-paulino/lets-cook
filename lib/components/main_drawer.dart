0d654060a0e377b5965173859b79f7e5f5486efa
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-14 21:24:20 -0300

    drawer menu and settings link

---
+++ lib/components/main_drawer.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../utils/app_routes.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget _createItem(IconData icon, String label, Function onTap) {
+    return ListTile(
+      leading: Icon(icon, size: 24),
+      title: Text(
+        label,
+        style: TextStyle(
+            fontFamily: 'RobotoCondensed',
+            fontSize: 20.0,
+            fontWeight: FontWeight.w600),
+      ),
+      onTap: onTap,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120.0,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).accentColor,
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              'Lets cook?',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30.0,
+                color: Colors.yellowAccent,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          _createItem(
+            Icons.restaurant,
+            'Recipes',
+            () => Navigator.of(context).pushNamed(AppRoutes.HOME)
+          ),
+          _createItem(
+            Icons.settings,
+            'Settings',
+            () => Navigator.of(context).pushNamed(AppRoutes.SETTINGS)
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
+import '../components/main_drawer.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -30,6 +31,7 @@ class _TabsScreenState extends State<TabsScreen> {
         ),
         centerTitle: true,
       ),
+      drawer: MainDrawer(),
       body: _screens[_selectedScreenIndex]['screen'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectScreen,
