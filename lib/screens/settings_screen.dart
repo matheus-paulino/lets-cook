d318a07e40b195d73c1e2ab7311f692085e093b1
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-15 21:29:11 -0300

    settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 
 import '../components/main_drawer.dart';
+import '../models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
-  const SettingsScreen({Key key}) : super(key: key);
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChanged,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +32,46 @@ class SettingsScreen extends StatelessWidget {
         title: Text('Settings'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Settings'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Settings',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _createSwitch(
+                  'No gluten',
+                  'Only gluten-free recipes.',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isGlutenFree = value),
+                ),
+                _createSwitch(
+                  'No gluten',
+                  'Only lactose-free recipes.',
+                  settings.isLactoseFree,
+                  (value) => setState(() => settings.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegan',
+                  'Only vegan recipes.',
+                  settings.isVegan,
+                  (value) => setState(() => settings.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetarian',
+                  'Only vegetarian recipes.',
+                  settings.isVegetarian,
+                  (value) => setState(() => settings.isVegetarian = value),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
