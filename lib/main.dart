ee783637b68d20b611b495e538e2b99efc04bbce
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-18 21:51:48 -0300

    resolved issue toggle button

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
+
   List<Meal> _availableMeals = DUMMY_MEALS;
 
   void _filterMeals(Settings settings) {
     setState(() {
+      this.settings = settings;
+
       _availableMeals = DUMMY_MEALS.where((meal) {
         final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
         final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
@@ -55,7 +59,7 @@ class _MyAppState extends State<MyApp> {
         AppRoutes.CATEGORIES_MEALS: (_) =>
             CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAIL: (_) => MealDetailSreen(),
-        AppRoutes.SETTINGS: (_) => SettingsScreen(_filterMeals)
+        AppRoutes.SETTINGS: (_) => SettingsScreen(_filterMeals, settings)
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
