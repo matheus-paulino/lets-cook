696b60fc6d00a4c283150404b5f08d1995470f15
matheus-paulino <matheuspaulino.santos1@gmail.com>
2021-03-18 21:37:20 -0300

    data via constructor available meals

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'screens/categories_meals_screen.dart';
 import 'utils/app_routes.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/errors/route_not_found_screen.dart';
+import 'models/meal.dart';
+import 'data/dummy_data.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +17,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<Meal> _availableMeals = DUMMY_MEALS;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +35,8 @@ class _MyAppState extends State<MyApp> {
       ),
       routes: {
         AppRoutes.HOME: (_) => TabsScreen(),
-        AppRoutes.CATEGORIES_MEALS: (_) => CategoriesMealsScreen(),
+        AppRoutes.CATEGORIES_MEALS: (_) =>
+            CategoriesMealsScreen(_availableMeals),
         AppRoutes.MEAL_DETAIL: (_) => MealDetailSreen(),
         AppRoutes.SETTINGS: (_) => SettingsScreen()
       },
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -3,8 +3,15 @@ import 'package:flutter/material.dart';
 import '../components/meal_component.dart';
 import '../models/category.dart';
 import '../data/dummy_data.dart';
+import '../models/meal.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
+  final List<Meal> meals;
+
+  const CategoriesMealsScreen(
+    this.meals,
+  );
+
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context).settings.arguments as Category;
